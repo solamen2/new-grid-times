4a02bdb4c92cc158d19cd1b08e943d4cb68c70f7
Edward Younskevicius <eyounskevicius@fullsail.com>
2023-03-17 15:04:01 -0700

    Do npm install, and implement Exercise 1: Header tweaks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,10 +10,10 @@ import Button from '../Button';
 
 const Header = () => {
   return (
-    <header>
+    <Wrapper>
       <SuperHeader>
         <Row>
-          <ActionGroup>
+          <ActionGroup style={{ '--padding-top': "8px"}}>
             <button>
               <Search size={24} />
             </button>
@@ -21,24 +21,67 @@ const Header = () => {
               <Menu size={24} />
             </button>
           </ActionGroup>
-          <ActionGroup>
-            <button>
+          <ActionGroup style={{ '--flex-direction': "column", '--align-items': "center", '--gap': "8px" }}>
+            <button id="userButton">
               <User size={24} />
             </button>
+            <Button id="subscribeButton">
+              SUBSCRIBE
+            </Button>
+            <Link href="/" id="subscribeLink">
+              Already a subscriber?
+            </Link>
           </ActionGroup>
         </Row>
       </SuperHeader>
       <MainHeader>
         <Logo />
       </MainHeader>
-    </header>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.header`{
+    isolation: isolate;
+  }
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  #subscribeButton {
+    display: none;
+  }
+
+  #subscribeLink {
+    display: none;
+  }
+
+  @media ${QUERIES.desktopAndUp} {
+    padding: 44px 0;
+    background: var(--white);
+    color: var(--color-gray-900);
+
+    #userButton {
+      display: none;
+    }
+
+    #subscribeButton {
+      display: revert;
+    }
+
+    #subscribeLink {
+      display: revert;
+    }
+  }
+`;
+
+const Link = styled.a`
+  font-family: var(--font-family-serif);
+  font-size: 0.875rem;
+  font-style: italic;
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -48,7 +91,14 @@ const Row = styled(MaxWidthWrapper)`
 
 const ActionGroup = styled.div`
   display: flex;
-  gap: 24px;
+  flex-direction: var(--flex-direction, rov);
+  align-items: var(--align-items, start);
+  gap: var(--gap, 24px);
+
+  @media ${QUERIES.desktopAndUp}
+  {
+    padding-top: var(--padding-top, 0);
+  }
 
   /*
     FIX: Remove the inline spacing that comes with
@@ -57,14 +107,26 @@ const ActionGroup = styled.div`
   svg {
     display: block;
   }
+
+  z-index: 2;
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 32px;
+  margin-top: 28px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 42px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.desktopAndUp} {
+    margin-top: -146px;
+    z-index: 1;
+  }
 `;
 
 export default Header;
