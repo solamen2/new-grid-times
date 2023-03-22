83b3b79e2eb5d4d067840af90c9bc4b1daa38f80
Edward Younskevicius <eyounskevicius@fullsail.com>
2023-03-22 11:09:37 -0700

    Implement Exercise 4: Specialty Stories grid

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -3,6 +3,8 @@ import styled from 'styled-components/macro';
 
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 
+import { QUERIES } from '../../constants';
+
 import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
@@ -34,11 +36,13 @@ const SpecialtyStoryGrid = () => {
         >
           Sports
         </SectionTitle>
-        <SportsStories>
-          {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
-          ))}
-        </SportsStories>
+        <SportsStoriesWrapper>
+          <SportsStories>
+            {SPORTS_STORIES.map((data) => (
+              <MiniStory key={data.id} {...data} />
+            ))}
+          </SportsStories>
+        </SportsStoriesWrapper>
       </SportsSection>
     </Wrapper>
   );
@@ -46,15 +50,60 @@ const SpecialtyStoryGrid = () => {
 
 const Wrapper = styled.div`
   display: grid;
+  grid-template-areas:
+    'markets-section'
+    'sports-section';
   gap: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-areas:
+      'markets-section sports-section';
+    grid-template-columns:
+      50% 50%;
+    gap: 0px;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  grid-area: markets-section;
 
-const MarketCards = styled.div``;
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+    border-right: solid 1px var(--color-gray-300);
+  }
+`;
 
-const SportsSection = styled.section``;
+const MarketCards = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns:
+    repeat(auto-fill, minmax(150px, 1fr));
+`;
 
-const SportsStories = styled.div``;
+const SportsSection = styled.section`
+  grid-area: sports-section;
+
+  @media ${QUERIES.laptopAndUp} {
+    padding-left: 16px;
+  }
+`;
+
+const SportsStoriesWrapper = styled.div`
+  overflow-x: auto;
+`;
+
+const SportsStories = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns:
+    repeat(auto-fill, minmax(150px, 1fr));
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-auto-flow: column;
+    grid-template-columns:
+      repeat(auto-fill, minmax(215px, 1fr));
+    grid-auto-columns: minmax(215px,1fr);
+  }
+`;
 
 export default SpecialtyStoryGrid;
